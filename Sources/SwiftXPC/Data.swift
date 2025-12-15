8f0253e53a42c2587539d426ac825ee2b1f8ecc7
ghostflyby <ghostflyby+git@outlook.com>
2025-12-15 18:34:54 +0800

    fix: correct import order in Data.swift

--- Sources/SwiftXPC/Data.swift
+++ Sources/SwiftXPC/Data.swift
@@ -1,7 +1,7 @@
 // SPDX-FileCopyrightText: 2025 ghostflyby
 // SPDX-License-Identifier: Apache-2.0
-import XPC
 import Foundation.NSData
+import XPC
 
 @frozen
 public enum XPCBool: XPCObject, @unchecked Sendable, RawRepresentable,

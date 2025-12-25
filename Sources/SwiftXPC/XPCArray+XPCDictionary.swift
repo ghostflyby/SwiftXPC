2c380b064f955094336a69fdd164fcc1ef062a36
ghostflyby <ghostflyby+git@outlook.com>
2025-12-25 20:59:03 +0800

    feat: XPCArray and XPCDictionary wrapper

---
+++ Sources/SwiftXPC/XPCArray+XPCDictionary.swift
@@ -0,0 +1,138 @@
+import XPC
+
+@frozen
+public struct XPCArray {
+  public let xpc_object: xpc_object_t
+  public init(xpc_object: xpc_object_t) {
+    self.xpc_object = xpc_object
+  }
+}
+
+extension XPCArray: XPCMarshal {
+  public func marshal() throws -> XPCObject {
+    XPCObject(xpc_object: xpc_object)
+  }
+  public static func unmarshal(from object: XPCObject) throws -> Self {
+    try ensureType(object, is: XPC_TYPE_ARRAY, for: XPCArray.self)
+    return XPCArray(xpc_object: object.xpc_object)
+  }
+}
+
+extension XPCArray: RandomAccessCollection {
+
+  public var startIndex: Int {
+    0
+  }
+
+  public var endIndex: Int {
+    Int(xpc_array_get_count(xpc_object))
+  }
+
+}
+
+extension XPCArray: MutableCollection {
+  public typealias Element = XPCObject
+
+  private func validateIndex(_ position: Int) {
+    precondition(position >= startIndex && position < endIndex, "Index out of bounds")
+  }
+
+  public subscript(position: Int) -> XPCObject {
+    get {
+      validateIndex(position)
+      let item = xpc_array_get_value(xpc_object, position)
+      return .init(xpc_object: item)
+    }
+    set {
+      validateIndex(position)
+      xpc_array_set_value(xpc_object, position, newValue.xpc_object)
+    }
+  }
+
+}
+
+extension XPCArray {
+  public mutating func append(_ obj: XPCObject) {
+    xpc_array_append_value(xpc_object, obj.xpc_object)
+  }
+}
+
+extension XPCArray {
+  public init() {
+    self.init(xpc_object: xpc_array_create_empty())
+  }
+}
+
+@frozen
+public struct XPCDictionary: @unchecked Sendable {
+  public let xpc_object: xpc_object_t
+  public init(xpc_object: xpc_object_t) {
+    self.xpc_object = xpc_object
+  }
+}
+
+extension XPCDictionary: XPCMarshal {
+  public func marshal() throws -> XPCObject {
+    XPCObject(xpc_object: xpc_object)
+  }
+  public static func unmarshal(from object: XPCObject) throws -> Self {
+    try ensureType(object, is: XPC_TYPE_DICTIONARY, for: XPCDictionary.self)
+    return XPCDictionary(xpc_object: object.xpc_object)
+  }
+}
+
+extension XPCDictionary {
+  public init?(replyTo message: XPCDictionary) {
+    if let reply = xpc_dictionary_create_reply(message.xpc_object) {
+      self.xpc_object = reply
+    } else {
+      return nil
+    }
+  }
+
+  public var connection: XPCConnection? {
+    if let conn = xpc_dictionary_get_remote_connection(xpc_object) {
+      return XPCConnection(xpc_object: conn)
+    } else {
+      return nil
+    }
+  }
+
+  var keys: [String] {
+    var result: [String] = []
+    xpc_dictionary_apply(xpc_object) { key, _ in
+      result.append(String(cString: key))
+      return true
+    }
+    return result
+  }
+
+  func contains(key: String) -> Bool {
+    xpc_dictionary_get_value(xpc_object, key) != nil
+  }
+
+  public init() {
+    self.xpc_object = xpc_dictionary_create(nil, nil, 0)
+  }
+}
+
+extension XPCDictionary {
+
+  public subscript(key: String) -> XPCObject? {
+    get {
+      if let item = xpc_dictionary_get_value(xpc_object, key) {
+        .init(xpc_object: item)
+      } else {
+        nil
+      }
+    }
+    set {
+      if let newValue = newValue {
+        xpc_dictionary_set_value(xpc_object, key, newValue.xpc_object)
+      } else {
+        xpc_dictionary_set_value(xpc_object, key, xpc_null_create())
+      }
+    }
+  }
+
+}
